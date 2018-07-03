40681ca3f9c1381902319061a087e990c1dd6ccd
zhourui@digisky.com <zhourui@digisky.com>
2018-07-03 17:25:20 +0800

    lua调用C#

--- lua_interface_for_CLR/lua_interface_for_CLR/Program.cs
+++ lua_interface_for_CLR/lua_interface_for_CLR/Program.cs
@@ -8,10 +8,27 @@ namespace lua_interface_for_CLR
 {
     class Program
     {
+        class TestReg {
+            private int value = 0;
+            public void TestRegFun(int num) {
+                Console.WriteLine("testreg.testregfun is running value=={0}",value = num);
+
+            }
+            public static void TestStatic() {
+                Console.WriteLine("test.teststatic is running");
+            }
+        }
         static void Main(string[] args)
         {
             // 新建一个Lua解释器，每一个Lua实例都相互独立
             Lua lua = new Lua();
+
+            TestReg obj = new TestReg();
+            lua.RegisterFunction("LuaTestRegFun", obj, obj.GetType().GetMethod("TestRegFun"));
+
+            //注册CLR静态方法到Lua中
+            // 也可用 typeof(TestReg).GetMethod("TestRegFun")
+            lua.RegisterFunction("LuaTestStatic", null, typeof(TestReg).GetMethod("TestStatic"));
 
             // Lua的索引操作[]可以创建、访问、修改global域，括号里面是变量名
             // 创建global域num和str
@@ -23,7 +40,7 @@ namespace lua_interface_for_CLR
 
             // 执行lua脚本，着两个方法都会返回object[]记录脚本的执行结果
             lua.DoString("num = 100; print(\"i am a lua string\")");
-            lua.DoFile("F:\\unity\\lua_interface_for_CLR\\lua_interface_for_CLR\\lua_script\\lua_interface_test.lua");
+            lua.DoFile("../../../lua_script/lua_interface_test.lua");
             object[] retVals = lua.DoString("return num,str");
 
             // 访问global域num和str
--- lua_interface_for_CLR/lua_script/lua_interface_test.lua
+++ lua_interface_for_CLR/lua_script/lua_interface_test.lua
@@ -1,2 +1,4 @@
 print('this is from lua file')
 print("C# num =="..num)
+LuaTestRegFun(99)
+LuaTestStatic()
